b23f929fbf7ecf3394660ad0a79e3dfd91364c27
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-04 21:24:11 +0000

    Formatted code + is running

--- queue_1.py
+++ queue_1.py
@@ -1,31 +1,34 @@
 '''Queue Data Structure to learn FIFO scenarios, OOP'''
 
 # Using a new class to implement a queue with functions
+
+
 class Queue:
-    def __init__(self): #initialize attributes of object(self)
+    def __init__(self):  # initialize attributes of object(self)
         self.items = []
-    
-    def __repr__(self): #return a string as rep of the object
+
+    def __repr__(self):  # return a string as rep of the object
         return f'Queue object: data={self.items}'
 
-    def is_empty(self): #return inverse boolean value
+    def is_empty(self):  # return inverse boolean value
         return not self.items
 
-    def enqueue(self, item): #add item to self.items
+    def enqueue(self, item):  # add item to self.items
         self.items.append(item)
 
-    def dequeue(self): #remove items from beginning
+    def dequeue(self):  # remove items from beginning
         return self.items.pop(0)
 
-    def size(self): #return the length of items
+    def size(self):  # return the length of items
         return len(self.items)
 
-    def peek(self): #return top element in self.items
+    def peek(self):  # return top element in self.items
         return self.items[0]
 
-#execute code as script
+
+# execute code as script
 if __name__ == '__main__':
-    q = Queue() #store queue class in this variable
+    q = Queue()  # store queue class in this variable
     print(q.is_empty())
     q.enqueue('First')
     q.enqueue('Second')
