43b3a71e289a40a0acf93ed1159ecb0c11e667a1
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-04 20:59:59 +0000

    use repr class obj as str

---
+++ queue_1.py
@@ -0,0 +1,9 @@
+'''Queue Data Structure to learn FIFO scenarios, OOP'''
+
+# Using a new class to implement a queue with functions
+class Queue:
+    def __init__(self): #initialize attributes of object(self)
+        self.items = []
+    
+    def __repr__(self): #return a string as rep of the object
+        return f'Queue object: data={self.items}'
