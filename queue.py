c5e3c33a71e741cfeedbab8fe25ea795dc90495c
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-04 20:52:09 +0000

    created class and init func

--- queue.py
+++ queue.py
@@ -1,1 +1,9 @@
 '''Queue Data Structure to learn FIFO scenarios, OOP'''
+
+# Using a new class to implement a queue
+
+
+class Queue:
+    # initialize attributes of object(self)
+    def __init__(self):
+        self.items = []
